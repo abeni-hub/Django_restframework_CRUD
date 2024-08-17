05105eff7c68d0d975edfe5f8a16d4aba87589be
Abenezer Sileshi <138670844+abeni-hub@users.noreply.github.com>
2024-08-17 16:59:16 +0300

    Update models.py

--- api/models.py
+++ api/models.py
@@ -5,12 +5,6 @@ class Item(models.Model):
     id = models.AutoField(primary_key= True)
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 100)
-    UNIT_CHOICES  =[
-        ('ltr' , 'Liter'),
-        ('kg' , 'Kilogram'),
-        ('pcs' , 'Pieces'),
-    ]
-    unit_of_measure = models.CharField(max_length = 10 , choices = UNIT_CHOICES)
     
     def __str__(self) -> str:
         return self.name
